6af977b86b1eeeb8837482121351c44f0de9a549
Matthew A. Mac Allister <mathandcsnerd@gmail.com>
2021-02-14 19:20:39 -0500

    added some missing gpl preambles

--- app/src/main/java/com/mathandcsnerd/random/MainActivity.java
+++ app/src/main/java/com/mathandcsnerd/random/MainActivity.java
@@ -1,3 +1,22 @@
+/* Copyright 2021 Matthew Macallister
+ *
+ * This file is part of MACSN Simple Rng App.
+ *
+ * MACSN Simple Rng App is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MACSN Simple Rng App is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MACSN Simple Rng App.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 package com.mathandcsnerd.random;
 
 import android.os.Bundle;
